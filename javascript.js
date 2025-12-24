6b89c3f2aa697513fcba697218863d29cc8fbc89
Rami Algamal <160735620+ramiyalgamal@users.noreply.github.com>
2025-12-24 15:24:06 +1100

    finished the project

--- javascript.js
+++ javascript.js
@@ -1,17 +1,90 @@
+// Function to get the computer choice
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-  let randomValue = Math.floor(Math.random() * 3);;
+  let randomValue = Math.floor(Math.random() * 3);
   if (randomValue === 0) {
-    console.log("0");
-    console.log("rock")
+    // console.log("0");
+    return "rock";
   }
   else if (randomValue === 1 ) {
     console.log("1");
-    console.log("paper")
+    return "paper";
   }
   else {
-    console.log("2")
-    console.log("scissors")
+    // console.log("2")
+    return "scissors";
   }
 }
 
-getComputerChoice()
+// Function to get the human choice
+function getHumanChoice() {
+  humanChoice = (prompt("Select “rock”, “paper” or “scissors”:")).toLowerCase();
+  return humanChoice
+}
+
+
+// Play a round
+function playRound(humanChoice, computerChoice){
+  if (humanChoice == "rock") {
+    if (computerChoice == "paper"){
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore += 1;
+    } else if (computerChoice == "scissors") {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    } else {
+      console.log(`its a draw! Computer chose ${computerChoice}`);
+    }
+  } 
+  else if (humanChoice == "paper") {
+    if (computerChoice == "paper"){
+      console.log(`its a draw! Computer chose ${computerChoice}`);
+    } else if (computerChoice == "scissors") {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore += 1;
+    } else {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    }
+  }
+  else {
+    if (computerChoice == "rock"){
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore += 1;
+    } else if (computerChoice == "paper") {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    } else {
+      console.log(`its a draw! Computer chose ${computerChoice}`);
+    }
+  }
+}
+
+
+
+function playGame() {
+  playRound(getHumanChoice(), getComputerChoice())
+  console.log(`Human Score: ${humanScore}`)
+  console.log(`Computer Score: ${computerScore}`)
+}
+
+let games = 5;
+
+while (games > 0) {
+  games -= 1;
+  playGame()
+}
+
+
+if (humanScore > computerScore) {
+  console.log(`Congrats, you won! Your score is: ${humanScore}, computer score ${computerScore}`)
+}
+else if (humanScore < computerScore) {
+  console.log(`You lost! Your score is: ${humanScore}, computer score ${computerScore}`)
+}
+else {
+  console.log(`its a draw! Your score is: ${humanScore}, computer score ${computerScore}`)
+}
